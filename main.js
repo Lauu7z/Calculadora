333f0a910d86b3e9f7d64ce136fa7d3afc920ae3
Laureano <laureanojuarez69@gmail.com>
2024-11-20 16:07:38 -0300

    Actualice funcionalidades

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
-const display = document.querySelector("#display");
-const buttons = document.querySelectorAll("button");
+const display = document.getElementById("display");
+const teclas = document.querySelectorAll(".teclas input");
 
-buttons.forEach((btn) => {
-  btn.addEventListener("click", () => {
-    if (btn.id === "equal") {
-      display.value = eval(display.value);
-    } else if (btn.id === "ac") {
-      display.value = "";
-    } else if (btn.id == "de") {
-      display.value = display.value.slice(0, -1);
+const agregar = (valor) => {
+  display.value += valor;
+};
+
+const borrar = () => {
+  display.value = "";
+};
+
+const borrarUltimo = () => {
+  display.value = display.value.slice(0, -1);
+};
+
+const calcular = () => {
+  const valorPantalla = display.value;
+  const resultado = eval(valorPantalla);
+  display.value = resultado;
+};
+
+teclas.forEach((tecla) => {
+  tecla.addEventListener("click", () => {
+    const valor = tecla.value;
+    if (valor === "AC") {
+      borrar();
+    } else if (valor === "DE") {
+      borrarUltimo();
+    } else if (valor === "=") {
+      calcular();
     } else {
-      display.value += btn.id;
+      agregar(valor);
     }
   });
 });
